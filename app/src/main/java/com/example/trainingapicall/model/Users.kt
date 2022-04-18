0336c48c5800b0734a231cdee5ca39463adb2ada
huyvqit <huyvq.it@gmail.com>
2022-04-18 15:06:13 +0700

    call api retrofit

--- app/src/main/java/com/example/trainingapicall/model/Users.kt
+++
@@ -1,3 +0,0 @@
-package com.example.trainingapicall.model
-
-class Users : ArrayList<UsersItem>()
--- app/src/main/java/com/example/trainingapicall/model/UsersItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.trainingapicall.model
-
-data class UsersItem(
-    val email: String,
-    val gender: String,
-    val id: Int,
-    val name: String,
-    val status: String
-)
